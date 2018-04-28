b884a4dd027847597b7e0b7c63b2132bc73e3fbe
WaiWarit <33046422+WaiWarit@users.noreply.github.com>
2018-04-29 00:04:19 +0700

    AddGuiMain

--- source/Main.Designer.vb
+++ source/Main.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Main
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,43 +20,128 @@ Partial Class Main
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.LFUBut = New System.Windows.Forms.Button()
         Me.LRUbut = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'LFUBut
         '
-        Me.LFUBut.Location = New System.Drawing.Point(355, 103)
+        Me.LFUBut.BackColor = System.Drawing.Color.FromArgb(CType(CType(80, Byte), Integer), CType(CType(250, Byte), Integer), CType(CType(123, Byte), Integer))
+        Me.LFUBut.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LFUBut.ForeColor = System.Drawing.Color.FromArgb(CType(CType(248, Byte), Integer), CType(CType(248, Byte), Integer), CType(CType(242, Byte), Integer))
+        Me.LFUBut.Location = New System.Drawing.Point(332, 287)
+        Me.LFUBut.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.LFUBut.Name = "LFUBut"
-        Me.LFUBut.Size = New System.Drawing.Size(400, 195)
+        Me.LFUBut.Size = New System.Drawing.Size(172, 86)
         Me.LFUBut.TabIndex = 0
-        Me.LFUBut.Text = "Button1"
-        Me.LFUBut.UseVisualStyleBackColor = True
+        Me.LFUBut.Text = "LFU"
+        Me.LFUBut.UseVisualStyleBackColor = False
         '
         'LRUbut
         '
-        Me.LRUbut.Location = New System.Drawing.Point(99, 132)
+        Me.LRUbut.BackColor = System.Drawing.Color.FromArgb(CType(CType(80, Byte), Integer), CType(CType(250, Byte), Integer), CType(CType(123, Byte), Integer))
+        Me.LRUbut.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LRUbut.ForeColor = System.Drawing.Color.FromArgb(CType(CType(248, Byte), Integer), CType(CType(248, Byte), Integer), CType(CType(242, Byte), Integer))
+        Me.LRUbut.Location = New System.Drawing.Point(811, 287)
+        Me.LRUbut.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.LRUbut.Name = "LRUbut"
-        Me.LRUbut.Size = New System.Drawing.Size(263, 234)
+        Me.LRUbut.Size = New System.Drawing.Size(172, 86)
         Me.LRUbut.TabIndex = 1
-        Me.LRUbut.Text = "LRUbut"
-        Me.LRUbut.UseVisualStyleBackColor = True
+        Me.LRUbut.Text = "LRU"
+        Me.LRUbut.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(248, Byte), Integer), CType(CType(248, Byte), Integer), CType(CType(242, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(458, 97)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(457, 116)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "LogoHere"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = Global.LFULRUSimulator.My.Resources.Resources.GitHub_Mark_Light_64px
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.PictureBox1.Location = New System.Drawing.Point(589, 507)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(129, 97)
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(424, 584)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 20)
+        Me.Label2.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(596, 620)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(111, 32)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Visit Us"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(85, Byte), Integer), CType(CType(85, Byte), Integer))
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(248, Byte), Integer), CType(CType(248, Byte), Integer), CType(CType(242, Byte), Integer))
+        Me.Button1.Location = New System.Drawing.Point(1143, 25)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(91, 50)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Exit"
+        Me.Button1.UseVisualStyleBackColor = False
         '
         'Main
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(40, Byte), Integer), CType(CType(42, Byte), Integer), CType(CType(54, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(1264, 744)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.LRUbut)
         Me.Controls.Add(Me.LFUBut)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(1286, 800)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(1286, 800)
         Me.Name = "Main"
-        Me.Text = "Main"
+        Me.Text = "LRUandLFU simulator"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents LFUBut As Button
     Friend WithEvents LRUbut As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
 End Class
--- source/Main.vb
+++ source/Main.vb
@@ -6,4 +6,26 @@
     Private Sub LRUbut_Click(sender As Object, e As EventArgs) Handles LRUbut.Click
         'LRU.ShowDialog()
     End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Dim webAddress As String = "https://github.com/unixxcorn/LRU-And-LFU-In-VB"
+        Process.Start(webAddress)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+
+    End Sub
 End Class
